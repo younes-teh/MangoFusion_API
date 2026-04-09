26956de87133b80b2c351475a0ff779ad2795dc6
younes-teh <younes.boualam66@gmail.com>
2026-04-09 17:43:10 +0000

    configuration et création des tables

---
+++ MangoFusion_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MangoFusion_API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+      public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+        
+           
+        }
+    }
+}
--- MangoFusion_API/Program.cs
+++ MangoFusion_API/Program.cs
@@ -1,6 +1,18 @@
+using MangoFusion_API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+  
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -12,6 +24,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
